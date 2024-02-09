1b36a4018688dc1971dae6b2822a6ce08892895a
Grayson <mxpklx@gmail.com>
2024-02-08 23:33:30 -0600

    Added start of jumping functionality

--- knight.gd
+++ knight.gd
@@ -9,6 +9,12 @@ class_name Knight
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var jumping = false
+var last_floor = true
+var attacks = [
+	"1h_slice_diagonal",
+	"1h_slice_horizontal",
+	"1h_attack_chop"
+]
 
 @onready var spring_arm = $SpringArm3D
 @onready var model = $Rig
@@ -20,8 +26,21 @@ func _physics_process(delta):
 	get_move_input(delta)
 	
 	move_and_slide()
-	if velocity.length() > 1.0:
+	if velocity.length() > 1.0 and (Input.is_action_pressed("forward") or Input.is_action_pressed("back")or Input.is_action_pressed("left")or Input.is_action_pressed("right")):
 		model.rotation.y = lerp_angle(model.rotation.y, spring_arm.rotation.y, rotation_speed * delta)
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = jump_speed
+		jumping = true
+		anim_tree.set("parameters/conditions/jumping", true)
+		anim_tree.set("parameters/conditions/grounded", false)
+	if is_on_floor() and not last_floor:
+		jumping = false
+		anim_tree.set("parameters/conditions/jumping", false)
+		anim_tree.set("parameters/conditions/grounded", true)
+	if not is_on_floor() and not jumping:
+		anim_tree.travel("Jump_Idle")
+		anim_tree.set("parameters/conditions/grounded", false)
+	last_floor = is_on_floor()
 	
 func get_move_input(delta):
 	var vy = velocity.y
@@ -29,6 +48,8 @@ func get_move_input(delta):
 	var input = Input.get_vector("left", "right", "forward", "back")
 	var dir = Vector3(input.x, 0, input.y).rotated((Vector3.UP), spring_arm.rotation.y)
 	velocity = lerp(velocity, dir * speed, acceleration * delta)
+	var vl = velocity * model.transform.basis
+	anim_tree.set("parameters/IWR/blend_position", Vector2(vl.x, -vl.z) / speed)
 	velocity.y = vy
 	
 func _unhandled_input(event):
@@ -36,3 +57,6 @@ func _unhandled_input(event):
 		spring_arm.rotation.x -= event.relative.y * mouse_sensitivity
 		spring_arm.rotation_degrees.x = clamp(spring_arm.rotation_degrees.x, -90.0, 30.0)
 		spring_arm.rotation.y -= event.relative.x * mouse_sensitivity
+	if event.is_action_pressed("attack"):
+		anim_state.travel(attacks.pick_random())
+
