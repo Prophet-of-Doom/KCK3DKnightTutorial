9f2cbf612cb4b0023a51232a772aa774f2ad8d79
Grayson <mxpklx@gmail.com>
2024-02-14 11:38:28 -0600

    Adding test scripts and scene

---
+++ test_scene.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_CONFINED_HIDDEN)
+	DisplayServer.mouse_set_mode(DisplayServer.MOUSE_MODE_CAPTURED)
+	 # Replace with function body.
